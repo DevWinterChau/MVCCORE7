e12b5db629512654396882432d9d9604f55044df
“DevwinterChau[D” <rangdong090302@gmail.com>
2023-12-17 23:52:44 +0700

    Cập nhật giao diện DanhMuc

--- MVC7/BAITAP/Areas/Admin/Controllers/DanhmucsController.cs
+++ MVC7/BAITAP/Areas/Admin/Controllers/DanhmucsController.cs
@@ -24,11 +24,28 @@ namespace BAITAP.Areas.Admin.Controllers
         }
 
         // GET: Danhmucs
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 8, string keyword = null, string category = null, string sort = null, bool Fill = false)
         {
-            return _context.Danhmucs != null ?
-                        View(await _context.Danhmucs.ToListAsync()) :
-                        Problem("Entity set 'ApplicationDbContext.Danhmucs'  is null.");
+            var applicationDbContext = await _context.Danhmucs.Include(x => x.Mathangs).ToListAsync();
+            var totalItems = applicationDbContext.Count();
+            // Filter by keyword if provided
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                applicationDbContext = applicationDbContext.Where(x => x.Ten.Contains(keyword.Trim())).ToList();
+            }
+            // Apply pagination
+            var items = applicationDbContext.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            // Tính toán các thông tin phân trang
+            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.keyword = keyword;
+            ViewBag.Fill = Fill;
+            // Populate the dropdown with categories
+            ViewBag.ListDanhMuc = await _context.CtKhuyenMais.ToListAsync();
+            return View(items);
         }
 
         // GET: Danhmucs/Details/5
